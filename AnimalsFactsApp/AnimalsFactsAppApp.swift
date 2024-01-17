775b5511f62a53e3c83f8ef853cd1e8ae2542225
Oleh Stasiv <o.stasiv@sweet.tv>
2024-01-17 20:34:57 +0200

    Initial commit

---
+++ AnimalsFactsApp/AnimalsFactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsFactsAppApp.swift
+//  AnimalsFactsApp
+//
+//  Created by Oleh Stasiv on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsFactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
